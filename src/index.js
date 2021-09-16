c9d902d093f210f1f33776bba7d7e5a2d744e2cc
GuidoFrassetti <frassetti.g@gmail.com>
2021-09-16 10:57:24 -0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-.logo{
-    text
+const listaCartas = ['src/img/cards/boo.jpg', 'src/img/cards/gogetassj4.jpg', 'src/img/cards/gokusjj.jpg','src/img/cards/janemba.jpg', 'src/img/cards/majinvegeta.jpg','src/img/cards/trunksssj.jpg','src/img/cards/vegetagt.jpg', 'src/img/cards/vegito.jpg']
+const cartas = document.querySelectorAll('.carta')
+repartirCartas()
+
+function repartirCartas()
+{
+    for (let i = 0; i < cartas.length; i++)
+    {
+        let srcCartaFrente = asiganarCarta()
+        cartas[i].src = srcCartaFrente       
+    }
+}
+
+function asiganarCarta()
+{
+
+    var cartadeFrente = listaCartas[Math.floor(Math.random() * listaCartas.length)];
+    return cartadeFrente
 }
