a552b06acaa65129930da5b94238b53b89c6e0ee
GuidoFrassetti <frassetti.g@gmail.com>
2021-09-19 03:13:40 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ const listaCartas = [{ id: "1", src: 'src/img/cards/boo.jpg' }, { id: "2", src: 
 const cartas = document.querySelectorAll('.carta')
 const intentos = []
 const puntaje = 0
+const srcCardBackwards = 'src/img/cards/2082113.png'
 mezclarCartas()
 
+//--------------------------------- -------------------------------------------------    -------------------------
 
 function mezclarCartas() {
     const cartasDuplicadas = listaCartas.concat(listaCartas)
@@ -20,37 +22,12 @@ function voltearCarta(e)
     const $cartaFrente = e.target
     const data = listaCartas.find(carta => carta.id === $cartaFrente.id) //find , es como un foreach, funcion flecha =>, puede llevar 3 params de los cuales retornara, elemento, indice, array
     $cartaFrente.src = data.src
+    setTimeout(function () {
+        $cartaFrente.src = srcCardBackwards
+    }, 2000)
 }
 
-/*
-function desbloquearInput()
-{
-    cartas.forEach(function($carta) {
-        $carta.onclick = inputUsuario
-    })
-}
-
-function inputUsuario(e)
-{
-    const $carta = e.target
-    console.log($carta)
-    mostrarCarta($carta)
-
-}
-
-function mostrarCarta($carta)
-{
-    let srcCartaFrente = asiganarCartaFrente()
-    $carta.src = srcCartaFrente
-}
-
-function asiganarCartaFrente()
-{
-
-    var cartadeFrente = listaCartas[Math.floor(Math.random() * listaCartas.length)];
-    return cartadeFrente
-}
-*/
+//------------------- ------------------------------------------------------------------------------------------
 for (let i = 0; i < cartas.length; i++)
 {
     var carta = cartas[i]
