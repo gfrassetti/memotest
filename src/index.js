76ce0c04cc9bd85d0548c03ac109239b76db472d
GuidoFrassetti <frassetti.g@gmail.com>
2021-09-20 02:28:45 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-const listaCartas = [{ id: "1", src: 'src/img/cards/boo.jpg' }, { id: "2", src: 'src/img/cards/gogetassj4.jpg' }, { id: "3", src: 'src/img/cards/gokusjj.jpg' }, { id: "4", src: 'src/img/cards/janemba.jpg' }, { id: "5", src: 'src/img/cards/majinvegeta.jpg' }, { id: "6", src: 'src/img/cards/trunksssj.jpg' }, { id: "7", src: 'src/img/cards/vegetagt.jpg' }, { id: "8", src: 'src/img/cards/vegito.jpg' }]
+const listaCartas = [{ name: "boo", id: "1", src: 'src/img/cards/boo.jpg' }, { name: "gogetassj4",id: "2", src: 'src/img/cards/gogetassj4.jpg' }, { name: "gokussj",id: "3", src: 'src/img/cards/gokusjj.jpg' }, {name: "janemba", id: "4", src: 'src/img/cards/janemba.jpg' }, {name: "majinvegeta", id: "5", src: 'src/img/cards/majinvegeta.jpg' }, {name: "trunksssj", id: "6", src: 'src/img/cards/trunksssj.jpg' }, {name: "vegetagt", id: "7", src: 'src/img/cards/vegetagt.jpg' }, { name: "vegito",id: "8", src: 'src/img/cards/vegito.jpg' }]
 
 const cartas = document.querySelectorAll('.carta')
-const intentos = []
-const puntaje = 0
+let intentos = 0
+let secuenciaTurno = []
+let puntaje = 0
 const srcCardBackwards = 'src/img/cards/2082113.png'
 mezclarCartas()
 
@@ -19,13 +20,49 @@ function mezclarCartas() {
 
 function voltearCarta(e)
 {
-    const $cartaFrente = e.target
-    const data = listaCartas.find(carta => carta.id === $cartaFrente.id) //find , es como un foreach, funcion flecha =>, puede llevar 3 params de los cuales retornara, elemento, indice, array
-    $cartaFrente.src = data.src
-    setTimeout(function () {
-        $cartaFrente.src = srcCardBackwards
-    }, 2000)
+    if (secuenciaTurno.length <= 2)
+    {
+        const $cartaFrente = e.target
+        const data = listaCartas.find(carta => carta.id === $cartaFrente.id) //find , es como un foreach, funcion flecha =>, puede llevar 3 params de los cuales retornara, elemento, indice, array
+        $cartaFrente.src = data.src
+        console.log(data)
+        secuenciaTurno.push($cartaFrente.id)
+        comprarCartas($cartaFrente)
+    }
+
 }
+
+function comprarCartas($cartaFrente)
+{
+    let primerItem = Number(secuenciaTurno[0])
+
+    let segundoItem = Number(secuenciaTurno[1])
+
+    if (secuenciaTurno.length === 2)
+    {
+        console.log(secuenciaTurno)
+        if (primerItem === segundoItem)
+        {
+            puntaje++;
+            intentos++;
+            console.log("Ambas se quedan asi")
+
+        }
+        else {
+            setTimeout(function () {
+                $cartaFrente.src = srcCardBackwards;                
+            }, 1000)
+            console.log("Deberian darse vuelta las dos")
+        }
+        vaciarCartasVolteadas()
+    }
+}
+
+function vaciarCartasVolteadas()
+{
+    secuenciaTurno = []
+}
+
 
 //------------------- ------------------------------------------------------------------------------------------
 for (let i = 0; i < cartas.length; i++)
