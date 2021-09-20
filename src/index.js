f2c85e84d864aa5c00ab4f9f3e8ab45fd3cd7cbc
GuidoFrassetti <frassetti.g@gmail.com>
2021-09-20 16:33:00 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const listaCartas = [{ name: "boo", id: "1", src: 'src/img/cards/boo.jpg' }, { name: "gogetassj4",id: "2", src: 'src/img/cards/gogetassj4.jpg' }, { name: "gokussj",id: "3", src: 'src/img/cards/gokusjj.jpg' }, {name: "janemba", id: "4", src: 'src/img/cards/janemba.jpg' }, {name: "majinvegeta", id: "5", src: 'src/img/cards/majinvegeta.jpg' }, {name: "trunksssj", id: "6", src: 'src/img/cards/trunksssj.jpg' }, {name: "vegetagt", id: "7", src: 'src/img/cards/vegetagt.jpg' }, { name: "vegito",id: "8", src: 'src/img/cards/vegito.jpg' }]
 
 const cartas = document.querySelectorAll('.carta')
-let intentos = 0
+let intentos = 0;
 let secuenciaTurno = []
-let puntaje = 0
-const srcCardBackwards = 'src/img/cards/2082113.png'
+const srcCartaTrasera = 'src/img/cards/2082113.png'
 mezclarCartas()
+perder()
 
 //--------------------------------- -------------------------------------------------    -------------------------
 
@@ -20,41 +20,51 @@ function mezclarCartas() {
 
 function voltearCarta(e)
 {
-    if (secuenciaTurno.length <= 2)
+    const $cartaFrente = e.target
+    if (secuenciaTurno.length < 2)
     {
-        const $cartaFrente = e.target
+
         const data = listaCartas.find(carta => carta.id === $cartaFrente.id) //find , es como un foreach, funcion flecha =>, puede llevar 3 params de los cuales retornara, elemento, indice, array
         $cartaFrente.src = data.src
-        console.log(data)
-        secuenciaTurno.push($cartaFrente.id)
+        console.log($cartaFrente)
+        secuenciaTurno.push($cartaFrente)
         comprarCartas($cartaFrente)
     }
-
+    else {
+        bloquearInput($cartaFrente)
+        console.log("Mas de dos")
+    }
+    console.log(secuenciaTurno.length)
 }
 
-function comprarCartas($cartaFrente)
+function comprarCartas()
 {
-    let primerItem = Number(secuenciaTurno[0])
+    let primerItem = secuenciaTurno[0]
 
-    let segundoItem = Number(secuenciaTurno[1])
+    let segundoItem = secuenciaTurno[1]
 
     if (secuenciaTurno.length === 2)
     {
         console.log(secuenciaTurno)
-        if (primerItem === segundoItem)
+        if (primerItem.id === segundoItem.id)
         {
-            puntaje++;
             intentos++;
             console.log("Ambas se quedan asi")
 
         }
         else {
+            intentos++;
             setTimeout(function () {
-                $cartaFrente.src = srcCardBackwards;                
+                primerItem.src = srcCartaTrasera
+                segundoItem.src = srcCartaTrasera
             }, 1000)
             console.log("Deberian darse vuelta las dos")
         }
-        vaciarCartasVolteadas()
+        setTimeout(function () {
+            vaciarCartasVolteadas() 
+        }, 1000)
+
+        intentosEnPantalla()
     }
 }
 
@@ -63,6 +73,31 @@ function vaciarCartasVolteadas()
     secuenciaTurno = []
 }
 
+function intentosEnPantalla()
+{
+    let elementoIntentos = document.querySelector('#intentos')
+    if (intentos !== 0 )
+    {
+        elementoIntentos.textContent = "Intentos: " + intentos
+    }
+}
+
+function bloquearInput($cartaFrente)
+{
+
+    $cartaFrente.removeEventListener("click", voltearCarta) 
+
+
+}
+
+function perder()
+{
+    if (intentos === 10)
+    {
+        console.log("Perdiste")
+        
+    }
+}
 
 //------------------- ------------------------------------------------------------------------------------------
 for (let i = 0; i < cartas.length; i++)
