15ae5993be312fee4bed4f5496d370bc742c3a06
GuidoFrassetti <frassetti.g@gmail.com>
2021-09-23 19:09:33 -0300

    Update memotest.js

--- cypress/integration/memotest.js
+++ cypress/integration/memotest.js
@@ -1,9 +1,10 @@
 
 context('Memotest', () => {
     before(() => {
-        cy.visit("http://127.0.0.1:8080")
+        cy.visit('http://127.0.0.1:8080')
     })
 });
+
 describe('Comienza el juego', () => {
     const NUMERO_CARTAS = 18;
     it('Verifica que haya un tablero con cartas', () => {
